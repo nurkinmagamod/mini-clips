23615b8741fa271ca6ac7030131c62f01755ed7e
Azamat Nurkin <azamat.nurkin@smddev.com>
2022-03-22 11:39:47 +0300

    start with assets

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    content: ['./src/**/*.{html,ts}'],
+  },
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
