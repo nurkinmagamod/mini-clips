472faf6c134caacc53bbbddc5f58123434d913b9
Azamat Nurkin <azamat.nurkin@smddev.com>
2022-03-24 15:10:41 +0300

    feat: work on modal

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
+import {ModalService} from "../../services/modal.service";
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
+  @Input() modalID = '';
 
-  constructor() { }
+  constructor(public modal: ModalService, public el: ElementRef) {}
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement)
   }
 
+  closeModal(): void {
+    this.modal.toggleModal(this.modalID)
+  }
 }
